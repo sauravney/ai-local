445f9209a72a110966cd260791cd4428d3886ec2
Miran Ahmad <222miran222@gmail.com>
2025-03-01 23:27:42 +0530

    loading skeleton

--- app/dashboard/_components/UserCourseList.jsx
+++ app/dashboard/_components/UserCourseList.jsx
@@ -33,13 +33,20 @@ function UserCourseList() {
     <div className="mt-10">
       <h2 className="font-medium text-xl">My AI Courses</h2>
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {courseList?.map((course, index) => (
-          <CourseCard
-            course={course}
-            key={index}
-            refreshData={() => getUserCourses()}
-          />
-        ))}
+        {courseList?.length > 0
+          ? courseList?.map((course, index) => (
+              <CourseCard
+                course={course}
+                key={index}
+                refreshData={() => getUserCourses()}
+              />
+            ))
+          : [1, 2, 3, 4, 5].map((item, index) => (
+              <div
+                key={index}
+                className="w-full mt-5 bg-slate-200 animate-pulse rounded-lg h-[270px]"
+              ></div>
+            ))}
       </div>
     </div>
   );
